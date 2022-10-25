9b0260192294fae9a193ce84485aefa94fa1e530
Bianca Oura <biancaoura@gmail.com>
2022-10-25 17:25:56 -0300

    req 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT
+	CONCAT(u.nome, ' ', u.sobrenome) AS usuario,
+    COUNT(h.musica_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(m.duracao_segundos) / 60, 2) AS total_minutos
+FROM usuarios AS u
+INNER JOIN historico_reproducao AS h
+	ON u.usuario_id = h.usuario_id
+INNER JOIN musicas AS m
+	ON m.musica_id = h.musica_id
+GROUP BY h.usuario_id
+ORDER BY u.nome ASC;
