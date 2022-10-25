6d30e9dca0363c0797a822eadeb6e7b280ff200f
Bianca Oura <biancaoura@gmail.com>
2022-10-25 17:51:41 -0300

    req 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+	CONCAT(u.nome, ' ', u.sobrenome) AS usuario,
+    IF(MAX(YEAR(h.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM SpotifyClone.usuarios AS u
+INNER JOIN historico_reproducao AS h
+	ON u.usuario_id = h.usuario_id
+GROUP BY h.usuario_id
+ORDER BY u.nome ASC;
