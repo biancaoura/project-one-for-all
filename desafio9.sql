2aa30cf3f28b247a7b83427707040576912475b4
Bianca Oura <biancaoura@gmail.com>
2022-10-25 18:41:42 -0300

    req 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT
+	COUNT(h.musica_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historico_reproducao AS h
+INNER JOIN SpotifyClone.usuarios AS u
+	ON u.usuario_id = h.usuario_id
+WHERE u.nome = 'Barbara'
+	AND u.sobrenome = 'Liskov';
